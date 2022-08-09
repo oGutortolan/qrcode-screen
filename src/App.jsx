9e5e17e66166f1bc56ed2ae543332fc6d7293380
oscargross <oscar.gross222@gmail.com>
2022-08-09 11:15:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [uuid, setUuid] = useState('')
+
+
+  return (
+    <div>
+      <p>teste2</p>
+    </div>
+  )
+}
+
+export default App
