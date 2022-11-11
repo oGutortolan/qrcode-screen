a5f6a82ff2184d744e880c4a69c4a9af864bc3d3
oscargross <oscar.gross222@gmail.com>
2022-11-11 10:22:01 -0300

    styles

--- src/components/QrCode.js
+++ src/components/QrCode.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import QRCode from 'qrcode';
 import { useParams } from 'react-router-dom';
-
+import logo from '../logo.png'
 const QrCode = () => {
     const [src, setSrc] = useState('')
     const { qrCode } = useParams()
@@ -15,11 +15,14 @@ const QrCode = () => {
             errorCorrectionLevel: 'H',
             type: 'image/jpeg',
             quality: 0.3,
+            width: 400,
+            height: 400,
             margin: 1,
-            color: {
-                dark: "#010599FF",
-                light: "#FFBF60FF"
-            }
+            borderRadius: '40px',
+            // color: {
+            //     dark: "white",
+            //     light: "white"
+            // }
         }, (err, url) => {
             if (err) throw Error() // qrcode generation error
             setSrc(url)
@@ -27,9 +30,73 @@ const QrCode = () => {
     }
 
     return (
-        <div>
-            <h1>QrCode</h1>
-            <img src={src}></img>
+
+        <div style={{
+            width: '100%',
+            height: '100%',
+            margin: 0,
+            padding: 0,
+            backgroundSize: 'cover',
+        }}>
+            <div style={{
+                height: '100%',
+                width: '100%',
+                position: 'absolute',
+                left: '50%',
+                top: '50%',
+                transform: 'translate(-50%, -50%)',
+                textAlign: 'center',
+            }}>
+                <div style={{
+                    position: 'absolute',
+                    left: '50%',
+                    top: '42%',
+                    transform: 'translate(-50%, -50%)',
+                    textAlign: 'center',
+                }}>
+                    <div style={{
+                        height: '240px',
+                    }}>
+                        <img style={{
+                            height: '300px',
+                            width: '300px',
+                        }}
+                            src={logo}>
+                        </img>
+                    </div>
+                    <div
+                        style={{
+                            backgroundColor: '#F7931E',
+                            height: '700px',
+                            width: '900px',
+                            // position: 'absolute',
+                            // left: '50%',
+                            // top: '50%',
+                            // transform: 'translate(-50%, -50%)',
+                            textAlign: 'center',
+                            borderRadius: '40px',
+                            paddingTop: '1px',
+                        }}                    >
+
+                        <h2 style={{
+                            textAlign: 'center',
+                            color: 'white',
+                            margin: '50px',
+                        }}>
+                            Use o QR Code para acessar o evento</h2>
+
+                        <img src={src}></img>
+
+                        <h1 style={{
+                            textAlign: 'center',
+                            color: 'white',
+                            margin: '50px',
+                        }}>
+                            Abra o app Ubeen e escaneie o QR Code para localizar o evento
+                        </h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
